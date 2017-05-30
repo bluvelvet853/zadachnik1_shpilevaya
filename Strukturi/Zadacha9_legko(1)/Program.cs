f4bb74ab06d4683a474ea3a7eefbe8b5c95ed930
black <black@DESKTOP-AEPC7MH>
2017-05-30 23:20:43 +0500

    30/05add

---
+++ Strukturi/Zadacha9_legko(1)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha9_legko_1_
+{
+    class Program
+    {
+        struct VScena
+        {
+            public string naimenovanie;
+            public int kolichestvo;
+            public string cena;
+            public int proizvoditel;
+            public string data_postupleniya_na_sklad;
+        }
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\black\Desktop\Strukturi9.txt");
+            string[] massiv = text.Split(new char[] { ':' });
+            VScena[] newmassiv = new VScena[massiv.Length];
+            for (int i = 0; i < massiv.Length - 1; i++)
+            {
+                string[] a = massiv[i].Split(new char[] { ',' });
+                newmassiv[i].naimenovanie = a[0];
+                newmassiv[i].kolichestvo = Convert.ToInt32(a[1]);
+                newmassiv[i].cena = Convert.ToInt32(a[2]);
+                newmassiv[i].proizvoditel = a[3];
+                newmassiv[i].data_postupleniya_na_sklad = Convert.ToInt32(a[4]);
+
+
+
+                {
+
+                    Console.WriteLine("Фамилия: {0}\r\n Дата Рождения: {1}\r\n Должность: {2}\r\n Зарплата: {3}\r\n Образование: {4}\r\n", newmassiv[i].naimenovanie = a[0], newmassiv[i].kolichestvo = Convert.ToInt32(a[1]), newmassiv[i].cena = Convert.ToInt32(a[2]), newmassiv[i].proizvoditel = a[3], newmassiv[i].data_postupleniya_na_sklad = Convert.ToInt32(a[4]));
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
+
---
+++ TextovieFaili/Zadacha22_legko/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha22_legko
+{
+    class Program
+    {
+        struct Plastinki
+        {
+            public string singer;
+            public int zhanr;
+            public string nazvanie;
+            public int tirazh;
+        }
+        static void Main(string[] args)
+        {
+        string text = System.IO.File.ReadAllText(@"C:\Users\black\Desktop\Ученый.txt");
+            string[] plastinki = text.Split(new char[] { ':' });
+
+            Plastinki[] plastinkiMas = new Plastinki[plastinki.Length];
+            for (int i = 0; i < plastinki.Length - 1; i++)
+            {
+                string[] a = plastinki[i].Split(new char[] { ';' });
+                plastinkiMas[i].singer = a[0];
+                plastinkiMas[i].zhanr = a[1];
+                plastinkiMas[i].nazvanie = a[2];
+                plastinkiMas[i].tirazh = Convert.ToInt32(a[3]);
+
+                if (plastinkiMas[i].tirazh > 10000)
+                {
+                    Console.WriteLine("Исполнитель: {0}\r\n Жанр: {1}\r\n Название: {2}\r\n Тираж: {3}\r\n", plastinkiMas[i].singer, plastinkiMas[i].zhanr = a[1], plastinkiMas[i].nazvanie = a[2], plastinkiMas[i].tirazh = Convert.ToInt32(a[3]));
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
